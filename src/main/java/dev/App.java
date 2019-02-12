79f77c83eb821929652dbf170ec8a18f6b8fec49
Lisa <maestrilisa@yahoo.fr>
2019-02-12 15:51:15 +0100

    tp3 configuration, profil, filtering

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,5 +1,7 @@
 package dev;
 
+import java.util.ResourceBundle;
+
 import com.github.lalyos.jfiglet.FigletFont;
 
 /**
@@ -10,7 +12,13 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println(FigletFont.convertOneLine( "Hello World!") );
-    
+       // System.out.println(FigletFont.convertOneLine( "Hello World!") );
+    	String titre = ResourceBundle.getBundle("application").getString("titre");
+    	String asciiArt = FigletFont.convertOneLine(titre);
+    	System.out.println(asciiArt);
+    	
+    	//affichage de l'environnement
+    	String environnement = ResourceBundle.getBundle("application").getString("environnement");
+    	System.out.println("Environnement : " + environnement);
     }
 }
